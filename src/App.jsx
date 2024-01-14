6b39edd666522820ee3e8aa1eff53f775520517a
sourey <sitaulasaurav@gmail.com>
2024-01-14 18:26:50 +0545

    shadcn components: Menubar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+
 import "./App.css";
 
 function App() {
-  return <h1>vite and @shadcn/ui</h1>;
+  return (
+    <>
+      <h1>vite and @shadcn/ui</h1>
+      <Menubar>
+        <MenubarMenu>
+          <MenubarTrigger>Hello Click Me </MenubarTrigger>
+          <MenubarContent>
+            <MenubarItem>My menu 1</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>My menu 2</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>My menu 3</MenubarItem>
+            <MenubarSeparator />
+          </MenubarContent>
+        </MenubarMenu>
+      </Menubar>
+    </>
+  );
 }
 
 export default App;
